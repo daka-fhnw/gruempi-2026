cc76f962ca9cde0ab37d607df42c61bdc94b30d3
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-01-26 20:54:43 +0100

    game rules added

--- src/pages/Regeln.tsx
+++ src/pages/Regeln.tsx
@@ -1,10 +1,161 @@
-import { Link } from "react-router-dom";
+import useScrollToTop from "../hooks/useScrollToTop";
 
 export default function Regeln() {
+  useScrollToTop();
   return (
     <div>
-      <h1>Regeln</h1>
-      <Link to="/">Start</Link>
+      <h1>Teilnahmebedingungen FHNW Grümpi 2026</h1>
+      <p>
+        In allen hier nicht aufgeführten Fällen entscheidet das OK endgültig.
+      </p>
+      <h2>Wetter</h2>
+      <ul>
+        <li>
+          Bei extremen Wetterverhältnissen wird das Turnier vom 8. Mai 2026 auf
+          den 15. Mai 2026 verschoben (Durchführungsentscheid bis spätestens 5.
+          Mai 2026).
+        </li>
+        <li>
+          Wenn während des Turniers extreme Platz- und Wetterverhältnissen
+          herrschen, kann das OK das Turnier unterbrechen oder abbrechen.
+        </li>
+      </ul>
+      <h2>Mannschaften</h2>
+      <ul>
+        <li>
+          Teilnahmeberechtigt sind alle Studierenden und Mitarbeitenden der
+          Fachhochschule Nordwestschweiz in Muttenz.
+        </li>
+        <li>
+          Eine Mannschaft besteht aus 5 Feldspieler:innen und einem:r
+          Torhüter:in sowie Auswechselspieler:innen. Eine Mannschaft darf aus
+          maximal 12 Spieler:innen bestehen.
+        </li>
+        <li>
+          Während der gesamten Spieldauer muss bei jeder Mannschaft mindestens
+          eine Dame am Spiel teilnehmen.
+        </li>
+        <li>
+          Jede:r Spieler:in darf nur in einer Mannschaft eingesetzt werden.
+          Verstösse führen zu Forfait Niederlagen oder zum Ausschluss aus dem
+          Turnier.
+        </li>
+        <li>
+          Pro Mannschaft ist eine Teilnahmegebühr von 30.- am Turniertag zu
+          bezahlen. Die Teilnahmegebühr muss vor Ort in Bar oder via Twint
+          bezahlt werden.
+        </li>
+        <li>
+          Jede Mannschaft hat einen Teamnamen, der nur aus alphanumerischen
+          Zeichen, Leerzeichen und Bindestrichen besteht. Irreguläre, vulgäre,
+          rassistische oder sexistische Teamnamen können vom OK abgewiesen
+          werden!
+        </li>
+        <li>
+          Jede Mannschaft muss in einem einheitlichen Tenue antreten (z.B.
+          einheitliche Farbe). Bei zu ähnlichen Tenues der aufeinandertreffenden
+          Mannschaften muss die zweitgenannte Mannschaft ein vom OK zur
+          Verfügung gestelltes Überziehleibchen tragen.
+        </li>
+        <li>
+          Jede Mannschaft muss sich spätestens 15 Minuten vor dem ersten Spiel
+          beim Wettkampfbüro melden und die Turniergebühr begleichen.
+        </li>
+      </ul>
+      <h2>Turniermodus</h2>
+      <ul>
+        <li>
+          Die gemeldeten Mannschaften werden in Gruppen aufgeteilt und spielen
+          jeweils einmal gegen jeden aus der eigenen Gruppe. Anschliessend
+          finden K.O.-Spiele statt. Je nach Anzahl Gruppen und Anzahl
+          Mannschaften wird entschieden wer sich für die K.O.-Runde
+          qualifiziert.
+        </li>
+        <li>
+          Die Rangliste wird nachfolgenden Kriterien gebildet:
+          <ol>
+            <li>Punktzahl</li>
+            <li>Direktbegegnung</li>
+            <li>Tordifferenz</li>
+            <li>Anzahl geschossene Tore</li>
+            <li>Münzwurf (beim Wettkampfbüro)</li>
+          </ol>
+        </li>
+      </ul>
+      <h2>Spielregeln</h2>
+      <ul>
+        <li>
+          Fairplay steht an diesem Anlass ganz oben. Es ist ein Spassturnier und
+          nicht die Weltmeisterschaft!!!
+        </li>
+        <li>Grundsätzlich wird nach den Regeln des SFV gespielt.</li>
+        <li>Abseits- und Rückpassregel sind aufgehoben.</li>
+        <li>
+          Spieldauer beträgt 10 Minuten ohne Seitenwechsel und ohne Pause.
+        </li>
+        <li>Die erstgenannte Mannschaft darf den Anstoss ausführen.</li>
+        <li>
+          Punkteverteilung:
+          <ul>
+            <li>Sieg: 3 Punkte</li>
+            <li>Unentschieden: je 1 Punkt</li>
+            <li>Niederlage: 0 Punkte</li>
+          </ul>
+        </li>
+        <li>
+          K.O.-Spiele werden bei Unentschieden nach 10 Minuten per
+          Penaltyschiessen entschieden. Jede:r Spieler:in muss einmal geschossen
+          haben, bevor diese ein zweites Mal antreten darf (auch Torhüter:in und
+          Ersatzspieler:innen). Es werden fünf Penalty pro Team ausgeführt,
+          falls noch keine Entscheidung gefallen ist, wird weitergespielt, bis
+          ein Team uneinholbar in Führung geht (Sudden Death).
+        </li>
+        <li>
+          Es sind nur Turnschuhe oder Nockenschuhe erlaubt. Keine Stollenschuhe!
+        </li>
+        <li>
+          Es können fliegend und unbegrenzt Auswechslungen getätigt werden.
+          Der/Die einzuwechselnde Spieler:in muss den Platz am selben Ort
+          betreten wie der/die auszuwechselnde Spieler:in ihn verlässt. Fouls
+          werden mit normalen Freistössen geahndet. In individuellen Fällen
+          entscheidet der Schiedsrichter.
+        </li>
+        <li>
+          Ein:e Spieler:in kann bei unsportlichem Verhalten oder groben Fouls
+          vom Feld verwiesen werden und für den Rest des Spiels gesperrt werden.
+          Das OK kann Spieler:innen oder ganze Mannschaften vom Turnier
+          ausschliessen!
+        </li>
+        <li>
+          Zu spät oder nicht antretende Mannschaften verlieren das betroffene
+          Spiel mit 0:3 Forfait.
+        </li>
+      </ul>
+      <h2>Schiedsrichter</h2>
+      <ul>
+        <li>
+          Jede Mannschaft muss während der Gruppenphase Schiedsrichter für
+          andere Spiele stellen.
+        </li>
+        <li>
+          Die Entscheidungen der Schiedsrichter sind endgültig und unanfechtbar.
+          Es gibt keinen VAR!
+        </li>
+      </ul>
+      <h2>Versicherung und Datenschutz</h2>
+      <ul>
+        <li>Versicherung ist Sache jedes einzelnen Teilnehmenden.</li>
+        <li>
+          Der Veranstalter lehnt für sich und seine Helfer:innen jegliche
+          Haftung gegenüber den Teilnehmenden ab!
+        </li>
+        <li>
+          Während dem gesamten Turnier werden Fotos gemacht. Mit der Anwesenheit
+          am Turnier akzeptierst du, dass Fotos von dir gemacht werden können
+          und auch auf unseren Sozialen Medien sowie der Website publiziert
+          werden dürfen.
+        </li>
+      </ul>
     </div>
   );
 }
