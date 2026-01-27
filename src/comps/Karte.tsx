7e4591d90fe4b38184bbacc377352ec857a91ce3
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-01-27 23:07:13 +0100

    multiple layers for map

--- src/comps/Karte.tsx
+++ src/comps/Karte.tsx
@@ -1,8 +1,20 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  LayersControl,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+} from "react-leaflet";
 import L from "leaflet";
 import marker from "../assets/marker.svg";
 import "./Karte.scss";
 
+const ATTRIBUTION =
+  '&copy; <a href="https://www.swisstopo.admin.ch/en/home.html">swisstopo</a>';
+const WMTS_MAP =
+  "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg";
+const WMTS_IMAGE =
+  "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.swissimage/default/current/3857/{z}/{x}/{y}.jpeg";
 const LOCATION = L.latLng([47.533354, 7.639526]);
 const BOUNDS = L.latLngBounds([45.81, 10.5], [47.81, 5.95]);
 const ICON = L.icon({
@@ -22,10 +34,14 @@ export default function Karte() {
       maxZoom={18}
       scrollWheelZoom={true}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.swisstopo.admin.ch/en/home.html">swisstopo</a>'
-        url="https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg"
-      />
+      <LayersControl position="topright">
+        <LayersControl.BaseLayer name="Karte" checked={true}>
+          <TileLayer attribution={ATTRIBUTION} url={WMTS_MAP} />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Luftbilder">
+          <TileLayer attribution={ATTRIBUTION} url={WMTS_IMAGE} />
+        </LayersControl.BaseLayer>
+      </LayersControl>
       <Marker position={LOCATION} icon={ICON}>
         <Popup>
           FHNW Grümpi 2026
