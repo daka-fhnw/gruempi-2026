15ce0753f152111d58b79d60170fa3962358ea88
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-02-05 13:58:43 +0100

    instant scroll to top by default

--- src/hooks/useScrollToTop.tsx
+++ src/hooks/useScrollToTop.tsx
@@ -1,7 +1,16 @@
 import { useEffect } from "react";
 
-const useScrollToTop = (): void => {
-  useEffect(() => window.scrollTo(0, 0), []);
+const useScrollToTop = (smooth: boolean = false): void => {
+  const behavior = smooth ? "smooth" : "instant";
+  useEffect(
+    () =>
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior,
+      }),
+    [behavior],
+  );
 };
 
 export default useScrollToTop;
