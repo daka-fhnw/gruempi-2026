1a6ee2346cd0d6d6e9e0cfcd4a11da4b0664b95a
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-02-05 20:41:25 +0100

    split pages into chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
+import React, { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Header from "./comps/Header";
-import Start from "./pages/Start";
-import Regeln from "./pages/Regeln";
-import Anreise from "./pages/Anreise";
-import Impressum from "./pages/Impressum";
-import Datenschutz from "./pages/Datenschutz";
+import Loading from "./comps/Loading";
 import "./App.scss";
 
 export default function App() {
+  const Start = React.lazy(() => import("./pages/Start"));
+  const Regeln = React.lazy(() => import("./pages/Regeln"));
+  const Anreise = React.lazy(() => import("./pages/Anreise"));
+  const Impressum = React.lazy(() => import("./pages/Impressum"));
+  const Datenschutz = React.lazy(() => import("./pages/Datenschutz"));
   return (
     <>
       <Header />
       <div className="wrapper d-flex flex-column">
         <main className="container flex-fill">
-          <Routes>
-            <Route path="/" element={<Start />} />
-            <Route path="/regeln" element={<Regeln />} />
-            <Route path="/anreise" element={<Anreise />} />
-            <Route path="/impressum" element={<Impressum />} />
-            <Route path="/datenschutz" element={<Datenschutz />} />
-          </Routes>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<Start />} />
+              <Route path="/regeln" element={<Regeln />} />
+              <Route path="/anreise" element={<Anreise />} />
+              <Route path="/impressum" element={<Impressum />} />
+              <Route path="/datenschutz" element={<Datenschutz />} />
+            </Routes>
+          </Suspense>
         </main>
         <footer className="bg-primary flex-grow-0">
           <div className="container text-light text-end">
---
+++ src/comps/Loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <div>lädt gerade...</div>;
+}
