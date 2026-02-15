4a51f55ade3738d27e906ca2503ad7d1520b79c6
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-02-15 17:05:45 +0100

    database connection prepared

--- public/test.php
+++ public/test.php
@@ -1,4 +1,24 @@
 <?php
 
-$config = include('lib/config.php');
-echo $config['db.host'];
+$config = require('lib/config.php');
+$dbconn = require('lib/dbconnect.php');
+
+$sql = "SELECT * FROM teams";
+$result = $dbconn->query($sql);
+
+if ($result->rowCount() > 0) {
+    echo "<table><tr><th>ID</th><th>Firstname</th><th>Lastname</th></tr>";
+    // Output data of each row
+    while ($row = $result->fetch()) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['firstname'] . "</td>";
+        echo "<td>" . $row['lastname'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    unset($result);
+} else {
+    echo "No records found.";
+}
+
