b455f14d874b9a4545176ed4a577aac48b23b944
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-02-16 17:34:23 +0100

    legal notice updated

--- src/pages/Datenschutz.tsx
+++ src/pages/Datenschutz.tsx
@@ -6,32 +6,33 @@ export default function Datenschutz() {
       <h1>Datenschutzerklärung</h1>
       <h2>Datenschutz</h2>
       <p>
-        Wir nehmen den Schutz deiner persönlichen Daten sehr ernst. Wir
-        behandeln deine personenbezogenen Daten vertraulich und entsprechend der
-        gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.
+        Die Betreiber dieser Webseite nehmen den Schutz Ihrer persönlichen Daten
+        sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und
+        entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser
+        Datenschutzerklärung.
       </p>
       <p>
-        Die Nutzung dieser Website ist ohne Angabe personenbezogener Daten
-        möglich. Soweit hier personenbezogene Daten (z.B. Name oder
-        E-Mail-Adresse) erhoben werden, erfolgt dies auf freiwilliger Basis. Die
-        erhobenen Daten werden ausschliesslich im direkten Zusammenhang mit dem
-        Grümpelturnier verwendet und somit niemandem ausserhalb zur Verfügung
-        gestellt oder verkauft. Nach Durchführung des Grümpelturniers werden
-        diese Daten vollständig gelöscht.
+        Die Nutzung dieser Webseite ist ohne Angabe personenbezogener Daten
+        möglich. Soweit auf unseren Seiten personenbezogene Daten (z.B. Name
+        oder E-Mail-Adresse) erhoben werden, erfolgt dies auf freiwilliger
+        Basis. Die erhobenen Daten werden ausschliesslich im direkten
+        Zusammenhang mit dem Grümpelturnier verwendet und somit niemandem
+        ausserhalb zur Verfügung gestellt oder verkauft. Nach Durchführung des
+        Grümpelturniers werden diese Daten vollständig gelöscht.
       </p>
       <h2>Cookies</h2>
-      <p>Diese Website verwendet keine Cookies.</p>
+      <p>Diese Webseite verwendet keine Cookies.</p>
       <h2>Hosting Provider & Server-Logfiles</h2>
       <p>
-        Der Provider dieser Website erhebt und speichert automatisch
-        Informationen in so genannten Server-Log-Files, die dein Browser
+        Der Provider dieser Webseite erhebt und speichert automatisch
+        Informationen in so genannten Server-Logfiles, die Ihr Browser
         automatisch übermittelt. Dies sind:
       </p>
       <ul>
         <li>IP-Adresse</li>
         <li>Browsertyp und Browserversion</li>
         <li>Verwendetes Betriebssystem</li>
-        <li>Referrer URL</li>
+        <li>Referrer-URL</li>
         <li>Hostname des zugreifenden Rechners</li>
         <li>Uhrzeit der Serveranfrage</li>
       </ul>
@@ -42,21 +43,21 @@ export default function Datenschutz() {
       </p>
       <h2>SSL-Verschlüsselung</h2>
       <p>
-        Diese Website nutzt aus Gründen der Sicherheit und zum Schutz der
-        Übertragung vertraulicher Inhalte, wie zum Beispiel Daten von dir, die
-        du an uns sendest, eine SSL-Verschlüsselung. Eine verschlüsselte
-        Verbindung erkennst du daran, dass die Adresszeile des Browsers mit
-        "https://" beginnt und am Schloss-Symbol in deiner Browserzeile. Dank
-        der SSL-Verschlüsselung können die Daten, die du an uns übermittelst,
-        nicht von Dritten mitgelesen werden.
+        Diese Webseite nutzt aus Gründen der Sicherheit und zum Schutz der
+        Übertragung vertraulicher Inhalte, wie zum Beispiel Daten von Ihnen, die
+        Sie an uns senden, eine SSL-Verschlüsselung. Eine verschlüsselte
+        Verbindung erkennen Sie daran, dass die Adresszeile des Browsers mit
+        "https://" beginnt und am Schloss-Symbol in Ihrer Browserzeile. Dank der
+        SSL-Verschlüsselung können die Daten, die Sie an uns übermitteln, nicht
+        von Dritten mitgelesen werden.
       </p>
       <h2>Recht auf Auskunft und Löschung</h2>
       <p>
-        Du hast jederzeit das Recht auf unentgeltliche Auskunft über deine
+        Sie haben jederzeit das Recht auf unentgeltliche Auskunft über Ihre
         gespeicherten personenbezogenen Daten, deren Herkunft und Empfänger und
         den Zweck der Datenverarbeitung sowie ein Recht auf Berichtigung oder
         Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema
-        personenbezogene Daten kannst du dich jederzeit unter der im{" "}
+        personenbezogene Daten können Sie sich jederzeit unter der im{" "}
         <Link to="/impressum">Impressum</Link> angegebenen Adresse an uns
         wenden.
       </p>
--- src/pages/Impressum.tsx
+++ src/pages/Impressum.tsx
@@ -1,3 +1,5 @@
+import { Link } from "wouter";
+
 export default function Impressum() {
   return (
     <>
@@ -11,6 +13,38 @@ export default function Impressum() {
       <h2>Kontakt</h2>
       <p>
         <a href="mailto:info@grümpi.ch">info@grümpi.ch</a>
+      </p>
+      <h2>Rechtliches</h2>
+      <h3>Haftung</h3>
+      <p>
+        Obwohl die Betreiber dieser Webseite mit aller Sorgfalt auf die
+        Richtigkeit der veröffentlichten Informationen achten, kann hinsichtlich
+        der inhaltlichen Richtigkeit, Genauigkeit, Aktualität, Zuverlässigkeit
+        und Vollständigkeit dieser Informationen keine Gewährleistung übernommen
+        werden. Die Betreiber dieser Webseite behalten es sich ausdrücklich vor,
+        Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu
+        verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise
+        oder endgültig einzustellen. Haftungsansprüche gegen die Betreiber
+        dieser Webseite wegen Schäden materieller oder immaterieller Art, welche
+        aus dem Zugriff oder der Nutzung bzw. Nichtnutzung der veröffentlichten
+        Informationen, durch Missbrauch der Verbindung oder durch technische
+        Störungen entstanden sind, werden ausgeschlossen.
+      </p>
+      <h3>Verweise und Links</h3>
+      <p>
+        Verweise und Links auf Webseiten Dritter liegen ausserhalb des
+        Verantwortungsbereichs der Betreiber dieser Webseite. Der Zugriff und
+        die Nutzung solcher Webseiten erfolgen auf eigene Gefahr des Nutzers
+        oder der Nutzerin. Die Betreiber dieser Webseite erklären ausdrücklich,
+        dass sie keinerlei Einfluss auf die Gestaltung, den Inhalt und die
+        Angebote der verknüpften Seiten haben. Informationen und
+        Dienstleistungen von verknüpften Webseiten liegen vollumfänglich in der
+        Verantwortung des jeweiligen Dritten. Es wird jegliche Verantwortung für
+        solche Webseiten abgelehnt.
+      </p>
+      <h3>Datenschutz</h3>
+      <p>
+        <Link href="/datenschutz">Datenschutzerklärung</Link>
       </p>
     </>
   );
