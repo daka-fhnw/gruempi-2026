b60cbb196092c8901eb8f609c86d73874a29fd4f
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-02-16 20:41:26 +0100

    404 image added

--- src/comps/NotFound.tsx
+++ src/comps/NotFound.tsx
@@ -1,9 +1,17 @@
 import { BackToStart } from "./BackToStart";
+import cat404 from "../assets/cat404.svg";
 
 export function NotFound() {
   return (
     <>
       <h1>Seite nicht gefunden!</h1>
+      <div className="mb-3">
+        <img
+          style={{ width: "100%", maxWidth: "350px" }}
+          src={cat404}
+          alt="404"
+        />
+      </div>
       <BackToStart />
     </>
   );
