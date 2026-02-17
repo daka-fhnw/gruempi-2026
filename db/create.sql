913decc0c9dae8764c27f849f8533cafa0b6fc6b
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-02-17 20:39:37 +0100

    database schema changed

--- db/create.sql
+++ db/create.sql
@@ -1,12 +1,30 @@
 CREATE TABLE
     teams (
-        id INT AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL UNIQUE,
-        email VARCHAR(255) NOT NULL,
-        token VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        verified_at TIMESTAMP NULL DEFAULT NULL,
-        deleted_at TIMESTAMP NULL DEFAULT NULL,
-        PRIMARY KEY (id)
+        `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        `team` VARCHAR(255) NOT NULL,
+        `email` VARCHAR(255) NOT NULL,
+        `firstname` VARCHAR(255) NOT NULL,
+        `lastname` VARCHAR(255) NOT NULL,
+        `mobile` VARCHAR(255),
+        `token` VARCHAR(255) NOT NULL,
+        `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `verified` BOOLEAN NOT NULL DEFAULT FALSE,
+        PRIMARY KEY (`id`),
+        UNIQUE INDEX (`team`),
+        UNIQUE INDEX (`email`),
+        INDEX `id_token` (`id`, `token`)
+    );
+
+CREATE TABLE
+    teams_log (
+        `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        `team_id` INT UNSIGNED NOT NULL,
+        `action` VARCHAR(20) NOT NULL,
+        `team` VARCHAR(255),
+        `email` VARCHAR(255),
+        `firstname` VARCHAR(255),
+        `lastname` VARCHAR(255),
+        `mobile` VARCHAR(255),
+        PRIMARY KEY (`id`)
     );
