f117b353da5dac661c97f54ab6af0aeb1e5d6ee8
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-02-18 22:02:05 +0100

    game rules updated

--- src/pages/Regeln.tsx
+++ src/pages/Regeln.tsx
@@ -1,75 +1,89 @@
 export default function Regeln() {
   return (
-    <div>
-      <h1>Teilnahmebedingungen</h1>
-      <p>
-        In allen hier nicht aufgeführten Fällen entscheidet das OK endgültig.
-      </p>
-      <h2>Wetter</h2>
+    <>
+      <h1>Teilnahmebedingungen und Spielregeln</h1>
+      <h2>1. Überblick</h2>
       <ul>
         <li>
-          Bei extremen Wetterverhältnissen wird das Turnier vom 8. Mai 2026 auf
-          den 15. Mai 2026 verschoben (Durchführungsentscheid bis spätestens 5.
-          Mai 2026).
+          <b>Maximale Anzahl Teams:</b> 20 Mannschaften
         </li>
         <li>
-          Wenn während des Turniers extreme Platz- und Wetterverhältnissen
-          herrschen, kann das OK das Turnier unterbrechen oder abbrechen.
+          <b>Gruppeneinteilung:</b> 4 Gruppen à 5 Teams.
+        </li>
+        <li>
+          <b>Qualifikation:</b> Die besten 2 Teams jeder Gruppe qualifizieren
+          sich für die K.O.-Runde (Viertelfinal).
+        </li>
+        <li>
+          <b>Finalspiele:</b> Ausgespielt wird nur das Finale (Platz 1/2), es
+          gibt kein Spiel um Platz 3/4.
         </li>
       </ul>
-      <h2>Mannschaften</h2>
+      <h2>2. Mannschaften</h2>
       <ul>
         <li>
           Teilnahmeberechtigt sind alle Studierenden und Mitarbeitenden der
-          Fachhochschule Nordwestschweiz in Muttenz.
+          Fachhochschule Nordwestschweiz in Muttenz (am Turnier Studi-Ausweis
+          vorweisen).
         </li>
         <li>
           Eine Mannschaft besteht aus 5 Feldspieler:innen und einem:r
-          Torhüter:in sowie Auswechselspieler:innen. Eine Mannschaft darf aus
-          maximal 12 Spieler:innen bestehen.
+          Torhüter:in sowie Auswechselspieler:innen. Pro Team sind maximal 12
+          Spieler:innen erlaubt.
         </li>
         <li>
-          Während der gesamten Spieldauer muss bei jeder Mannschaft mindestens
-          eine Dame am Spiel teilnehmen.
+          Während des Spiels ist es nicht zwingend, dass eine Dame auf dem Feld
+          steht, Tore von Damen werden jedoch doppelt gewertet.
         </li>
         <li>
           Jede:r Spieler:in darf nur in einer Mannschaft eingesetzt werden.
-          Verstösse führen zu Forfait Niederlagen oder zum Ausschluss aus dem
+          Verstösse führen zu Forfait-Niederlagen oder zum Ausschluss aus dem
           Turnier.
         </li>
         <li>
-          Pro Mannschaft ist eine Teilnahmegebühr von 30.- am Turniertag zu
-          bezahlen. Die Teilnahmegebühr muss vor Ort in Bar oder via Twint
-          bezahlt werden.
+          Pro Mannschaft ist eine Teilnahmegebühr von CHF 30.– am Turniertag zu
+          bezahlen (Bar oder via TWINT).
         </li>
         <li>
           Jede Mannschaft hat einen Teamnamen, der nur aus alphanumerischen
           Zeichen, Leerzeichen und Bindestrichen besteht. Irreguläre, vulgäre,
           rassistische oder sexistische Teamnamen können vom OK abgewiesen
-          werden!
+          werden.
         </li>
         <li>
-          Jede Mannschaft muss in einem einheitlichen Tenue antreten (z.B.
-          einheitliche Farbe). Bei zu ähnlichen Tenues der aufeinandertreffenden
-          Mannschaften muss die zweitgenannte Mannschaft ein vom OK zur
-          Verfügung gestelltes Überziehleibchen tragen.
+          Jede Mannschaft muss in einem einheitlichen Tenue antreten (z. B.
+          einheitliche Farbe). Bei zu ähnlichen Tenues muss die zweitgenannte
+          Mannschaft ein vom OK zur Verfügung gestelltes Überziehleibchen
+          tragen.
         </li>
         <li>
           Jede Mannschaft muss sich spätestens 15 Minuten vor dem ersten Spiel
           beim Wettkampfbüro melden und die Turniergebühr begleichen.
         </li>
       </ul>
-      <h2>Turniermodus</h2>
+      <h2>3. Turniermodus</h2>
       <ul>
         <li>
-          Die gemeldeten Mannschaften werden in Gruppen aufgeteilt und spielen
-          jeweils einmal gegen jeden aus der eigenen Gruppe. Anschliessend
-          finden K.O.-Spiele statt. Je nach Anzahl Gruppen und Anzahl
-          Mannschaften wird entschieden wer sich für die K.O.-Runde
-          qualifiziert.
+          <b>Gruppenphase</b>
+          <ul>
+            <li>
+              Die gemeldeten Mannschaften werden in 4 Gruppen aufgeteilt und
+              spielen jeweils einmal gegen jedes Team aus der eigenen Gruppe.
+            </li>
+          </ul>
         </li>
         <li>
-          Die Rangliste wird nachfolgenden Kriterien gebildet:
+          <b>K.O.-Phase</b>
+          <ul>
+            <li>
+              Die Top 2 jeder Gruppe (insgesamt 8 Teams) qualifizieren sich für
+              den Viertelfinal.
+            </li>
+            <li>Anschliessend folgen Halbfinals.</li>
+          </ul>
+        </li>
+        <li>
+          <b>Rangliste in der Gruppenphase</b>
           <ol>
             <li>Punktzahl</li>
             <li>Direktbegegnung</li>
@@ -79,11 +93,11 @@ export default function Regeln() {
           </ol>
         </li>
       </ul>
-      <h2>Spielregeln</h2>
+      <h2>4. Spielregeln</h2>
       <ul>
         <li>
           Fairplay steht an diesem Anlass ganz oben. Es ist ein Spassturnier und
-          nicht die Weltmeisterschaft!!!
+          nicht die Weltmeisterschaft.
         </li>
         <li>Grundsätzlich wird nach den Regeln des SFV gespielt.</li>
         <li>Abseits- und Rückpassregel sind aufgehoben.</li>
@@ -99,13 +113,19 @@ export default function Regeln() {
             <li>Niederlage: 0 Punkte</li>
           </ul>
         </li>
+        <li>Entscheidung in K.O.-Spielen</li>
         <li>
-          K.O.-Spiele werden bei Unentschieden nach 10 Minuten per
-          Penaltyschiessen entschieden. Jede:r Spieler:in muss einmal geschossen
-          haben, bevor diese ein zweites Mal antreten darf (auch Torhüter:in und
-          Ersatzspieler:innen). Es werden fünf Penalty pro Team ausgeführt,
-          falls noch keine Entscheidung gefallen ist, wird weitergespielt, bis
-          ein Team uneinholbar in Führung geht (Sudden Death).
+          Bei Unentschieden nach 10 Minuten wird per Penaltyschiessen
+          entschieden.
+        </li>
+        <li>
+          Jede:r Spieler:in muss einmal geschossen haben, bevor jemand ein
+          zweites Mal antreten darf (inkl. Torhüter:in und Ersatzspieler:innen).
+        </li>
+        <li>
+          Es werden zuerst 3 Penalty pro Team ausgeführt. Falls noch keine
+          Entscheidung gefallen ist, wird im Sudden Death weitergeschossen, bis
+          ein Team uneinholbar in Führung geht.
         </li>
         <li>
           Es sind nur Turnschuhe oder Nockenschuhe erlaubt. Keine Stollenschuhe!
@@ -113,22 +133,24 @@ export default function Regeln() {
         <li>
           Es können fliegend und unbegrenzt Auswechslungen getätigt werden.
           Der/Die einzuwechselnde Spieler:in muss den Platz am selben Ort
-          betreten wie der/die auszuwechselnde Spieler:in ihn verlässt. Fouls
-          werden mit normalen Freistössen geahndet. In individuellen Fällen
-          entscheidet der Schiedsrichter.
+          betreten, wie der/die auszuwechselnde Spieler:in ihn verlässt.
+        </li>
+        <li>
+          Fouls werden mit normalen Freistössen geahndet. In individuellen
+          Fällen entscheidet der Schiedsrichter.
         </li>
         <li>
           Ein:e Spieler:in kann bei unsportlichem Verhalten oder groben Fouls
           vom Feld verwiesen werden und für den Rest des Spiels gesperrt werden.
           Das OK kann Spieler:innen oder ganze Mannschaften vom Turnier
-          ausschliessen!
+          ausschliessen.
         </li>
         <li>
           Zu spät oder nicht antretende Mannschaften verlieren das betroffene
           Spiel mit 0:3 Forfait.
         </li>
       </ul>
-      <h2>Schiedsrichter</h2>
+      <h2>5. Schiedsrichter</h2>
       <ul>
         <li>
           Jede Mannschaft muss während der Gruppenphase Schiedsrichter für
@@ -139,12 +161,19 @@ export default function Regeln() {
           Es gibt keinen VAR!
         </li>
       </ul>
-      <h2>Versicherung und Datenschutz</h2>
+      <h2>6. Wetter</h2>
+      <ul>
+        <li>
+          Wenn während des Turniers extreme Platz- und Wetterverhältnissen
+          herrschen, kann das OK das Turnier unterbrechen oder abbrechen.
+        </li>
+      </ul>
+      <h2>7. Versicherung und Datenschutz</h2>
       <ul>
         <li>Versicherung ist Sache jedes einzelnen Teilnehmenden.</li>
         <li>
           Der Veranstalter lehnt für sich und seine Helfer:innen jegliche
-          Haftung gegenüber den Teilnehmenden ab!
+          Haftung gegenüber den Teilnehmenden ab.
         </li>
         <li>
           Während dem gesamten Turnier werden Fotos gemacht. Mit der Anwesenheit
@@ -153,6 +182,6 @@ export default function Regeln() {
           werden dürfen.
         </li>
       </ul>
-    </div>
+    </>
   );
 }
