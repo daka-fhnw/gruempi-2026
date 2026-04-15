a1c09fbcf7b23a770bb88bbaf744e9c81ff468ff
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-04-15 18:31:29 +0200

    game table improved

--- src/comps/SpielTabelle.tsx
+++ src/comps/SpielTabelle.tsx
@@ -10,44 +10,72 @@ interface SpielTabelleProps {
 export function SpielTabelle({ spiele, resolveName }: SpielTabelleProps) {
   return (
     <>
-      <div className="row g-2">
-        {spiele.map((spiel, index) => (
-          <div key={index} className="col-12 col-lg-6">
-            <Card className="shadow bg-white">
-              <Card.Body>
-                <div className="overflow-hidden">
-                  <div className="d-flex flex-nowrap gap-2 gap-sm-3">
-
-                    <div className="fw-bold">
-                      <div className="spiel-text">Uhrzeit:</div>
-                      <div className="spiel-text">Heimteam:</div>
-                      <div className="spiel-text">Gastteam:</div>
-                      <div className="spiel-text d-block d-sm-none">
-                        Schiri:
+      <Card className="shadow bg-white">
+        <Card.Body className="p-0">
+          <table className="table table-striped bg-white rounded-3 overflow-hidden spiel-table">
+            <thead>
+              <tr className="d-none d-md-table-row">
+                <th className="d-md-none">Spiele</th>
+                <th className="d-none d-md-table-cell">
+                  <span className="d-lg-none">Zeit, Platz</span>
+                  <span className="d-none d-lg-table-cell">Uhrzeit</span>
+                </th>
+                <th className="d-none d-lg-table-cell">Platz</th>
+                <th className="d-none d-md-table-cell">
+                  <span className="d-lg-none">Teams</span>
+                  <span className="d-none d-lg-table-cell">Heimteam</span>
+                </th>
+                <th className="d-none d-lg-table-cell">Gastteam</th>
+                <th className="d-none d-md-table-cell">Schiedsrichter</th>
+              </tr>
+            </thead>
+            <tbody>
+              {spiele.map((spiel, index) => (
+                <tr key={index}>
+                  <td className="d-md-none">
+                    <div className="spiel-small">
+                      <div className="fw-bold">Zeit, Platz:</div>
+                      <div>
+                        {spiel.zeit}, {spiel.platz}
                       </div>
-                      <div className="spiel-text d-none d-sm-block">
-                        Schiedsrichter:
+                      <div className="fw-bold">Heimteam:</div>
+                      <div>{resolveName(spiel.team1)}</div>
+                      <div className="fw-bold">Gastteam:</div>
+                      <div>{resolveName(spiel.team2)}</div>
+                      <div className="fw-bold">
+                        <span className="d-inline d-sm-none">Schiri:</span>
+                        <span className="d-none d-sm-inline">
+                          Schiedsrichter:
+                        </span>
                       </div>
+                      <div>{resolveName(spiel.schiri)}</div>
                     </div>
-                    <div className="flex-fill">
-                      <div className="spiel-text">{spiel.zeit} </div>
-                      <div className="spiel-text">
-                        {resolveName(spiel.team1)}
-                      </div>
-                      <div className="spiel-text">
-                        {resolveName(spiel.team2)}
-                      </div>
-                      <div className="spiel-text">
-                        {resolveName(spiel.schiri)}
-                      </div>
+                  </td>
+                  <td className="d-none d-md-table-cell">
+                    {spiel.zeit}
+                    <div className="d-none d-md-table-cell d-lg-none">
+                      {spiel.platz}
                     </div>
-                  </div>
-                </div>
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
-      </div>
+                  </td>
+                  <td className="d-none d-lg-table-cell">{spiel.platz}</td>
+                  <td className="d-none d-md-table-cell">
+                    {resolveName(spiel.team1)}
+                    <div className="d-none d-md-table-cell d-lg-none">
+                      {resolveName(spiel.team2)}
+                    </div>
+                  </td>
+                  <td className="d-none d-lg-table-cell">
+                    {resolveName(spiel.team2)}
+                  </td>
+                  <td className="d-none d-md-table-cell">
+                    {resolveName(spiel.schiri)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Card.Body>
+      </Card>
     </>
   );
 }
