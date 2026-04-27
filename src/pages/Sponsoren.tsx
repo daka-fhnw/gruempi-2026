4bcaac6309186b0f83d82a0862e66a2b48a1691d
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-04-27 20:38:43 +0200

    menu points updated

--- src/pages/Sponsoren.tsx
+++ src/pages/Sponsoren.tsx
@@ -13,37 +13,57 @@ export default function Sponsoren() {
     return (
       <div className="mb-n3">
         <h1>Sponsoren</h1>
-        <h2>Triangulationspunkt (Hauptsponsoren)</h2>
-        <SponsorGruppe
-          list={sponsoren.triangulationspunkte}
-          size="22rem"
-          fontSize="1.1rem"
-        />
-        <h2>Fixpunkt</h2>
-        <SponsorGruppe
-          list={sponsoren.fixpunkte}
-          size="18rem"
-          fontSize="1rem"
-        />
-        <h2>Höhenfixpunkt</h2>
-        <SponsorGruppe
-          list={sponsoren.hoehenfixpunkte}
-          size="14rem"
-          fontSize="0.9rem"
-        />
-        <h2>Passpunkt</h2>
-        <SponsorGruppe
-          list={sponsoren.passpunkte}
-          size="10.8rem"
-          fontSize="0.8rem"
-        />
-        <h2>Grenzpunkt</h2>
-        {sponsoren.grenzpunkte.map((sponsor, index) => (
-          <span key={index}>
-            {index !== 0 ? " | " : ""}
-            <MaybeWithUrl url={sponsor.url}>{sponsor.label}</MaybeWithUrl>
-          </span>
-        ))}
+        {sponsoren.triangulationspunkte.length > 0 && (
+          <>
+            <h2>Triangulationspunkt (Hauptsponsoren)</h2>
+            <SponsorGruppe
+              list={sponsoren.triangulationspunkte}
+              size="22rem"
+              fontSize="1.1rem"
+            />
+          </>
+        )}
+        {sponsoren.fixpunkte.length > 0 && (
+          <>
+            <h2>Fixpunkt</h2>
+            <SponsorGruppe
+              list={sponsoren.fixpunkte}
+              size="18rem"
+              fontSize="1rem"
+            />
+          </>
+        )}
+        {sponsoren.hoehenfixpunkte.length > 0 && (
+          <>
+            <h2>Höhenfixpunkt</h2>
+            <SponsorGruppe
+              list={sponsoren.hoehenfixpunkte}
+              size="14rem"
+              fontSize="0.9rem"
+            />
+          </>
+        )}
+        {sponsoren.passpunkte.length > 0 && (
+          <>
+            <h2>Passpunkt</h2>
+            <SponsorGruppe
+              list={sponsoren.passpunkte}
+              size="11rem"
+              fontSize="0.8rem"
+            />
+          </>
+        )}
+        {sponsoren.grenzpunkte.length > 0 && (
+          <>
+            <h2>Grenzpunkt</h2>
+            {sponsoren.grenzpunkte.map((sponsor, index) => (
+              <span key={index}>
+                {index !== 0 ? " | " : ""}
+                <MaybeWithUrl url={sponsor.url}>{sponsor.label}</MaybeWithUrl>
+              </span>
+            ))}
+          </>
+        )}
       </div>
     );
   } else {
