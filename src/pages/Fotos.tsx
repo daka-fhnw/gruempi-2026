1e5a78f5b902ecf36bc84042a452739b271ee17e
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2026-05-04 22:23:42 +0200

    photo album improved

--- src/pages/Fotos.tsx
+++ src/pages/Fotos.tsx
@@ -4,33 +4,30 @@ import { RowsPhotoAlbum } from "react-photo-album";
 import "react-photo-album/rows.css";
 
 import Lightbox from "yet-another-react-lightbox";
-import "yet-another-react-lightbox/styles.css";
-
 import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import Download from "yet-another-react-lightbox/plugins/download";
 import Counter from "yet-another-react-lightbox/plugins/counter";
+import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/plugins/counter.css";
 
-import { fotosGross, fotosKlein } from "../listen_fotos";
-import { Link } from "wouter";
 import { ArrowLink } from "../comps/ArrowLink";
+import { fotosGross, fotosKlein } from "../listen_fotos";
 
 export default function Fotos() {
   const [index, setIndex] = useState(-1);
-  console.log(fotosKlein);
   return (
     <>
       <h1>Fotos Grümpi 2026</h1>
       <div className="mb-3">
-        <Link
+        <a
           href="/data/Fotos_Gruempi_2026.zip"
           download="Fotos Grümpi 2026.zip"
         >
-          <ArrowLink>Alle Fotos herunterladen</ArrowLink>
-        </Link>{" "}
+          <ArrowLink>Alle Fotos herunterladen (528 MB)</ArrowLink>
+        </a>{" "}
         🤩
       </div>
       <RowsPhotoAlbum
